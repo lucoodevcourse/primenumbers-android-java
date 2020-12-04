878c1b125aa3a174f886de812d0463fda10e8ea5
Konstantin Läufer <klaeufer@gmail.com>
2020-12-03 20:42:37 -0600

    switched from Supplier to BooleanSupplier
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- app/src/main/java/edu/luc/etl/cs313/android/primechecker/util/PrimeCheckerUtil.java
+++ app/src/main/java/edu/luc/etl/cs313/android/primechecker/util/PrimeCheckerUtil.java
@@ -1,7 +1,7 @@
 package edu.luc.etl.cs313.android.primechecker.util;
 
 import java.util.function.IntConsumer;
-import java.util.function.Supplier;
+import java.util.function.BooleanSupplier;
 
 public enum PrimeCheckerUtil {;
 
@@ -9,13 +9,13 @@ public enum PrimeCheckerUtil {;
         return isPrimeLocal(i, () -> false, (final int k) -> { });
     }
 
-    public static boolean isPrimeLocal(final long i, final Supplier<Boolean> isCancelled, final IntConsumer publishProgress) { // optimized non-Async/local isPrime method
+    public static boolean isPrimeLocal(final long i, final BooleanSupplier isCancelled, final IntConsumer publishProgress) { // optimized non-Async/local isPrime method
         if (i < 2) return false;
         if (i == 2) return true;
         if (i % 2 == 0) return false;
         final long sqrt = Math.round(Math.sqrt(i));
         for (long k = 3; k <= sqrt; k += 2) {
-            if (isCancelled.get() || i % k == 0) return false;
+            if (isCancelled.getAsBoolean() || i % k == 0) return false;
             publishProgress.accept((int) (k * 100 / sqrt));
         }
         return true;
@@ -26,11 +26,11 @@ public enum PrimeCheckerUtil {;
     }
 
     // begin-method-isPrimeLong
-    public static boolean isPrimeOrig(final long i, final Supplier<Boolean> isCancelled, final IntConsumer publishProgress) { // original isPrime, now used for Async execution
+    public static boolean isPrimeOrig(final long i, final BooleanSupplier isCancelled, final IntConsumer publishProgress) { // original isPrime, now used for Async execution
         if (i < 2) return false;
         final long half = i / 2;
         for (long k = 2; k <= half; k += 1) {
-            if (isCancelled.get() || i % k == 0) return false;
+            if (isCancelled.getAsBoolean() || i % k == 0) return false;
             publishProgress.accept((int) (k * 100 / half));
         }
         return true;
